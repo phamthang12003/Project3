27bd08dc81be5593f6291dafd855f15c6eabbbe7
Thang <phamthang12003@gmail.com>
2023-07-31 11:50:36 +0700

    commit

---
+++ E-PROJECT_MANAGER/Repository/ViTriTuyenDungRepository.cs
@@ -0,0 +1,16 @@
+﻿using E_PROJECT_MANAGER.Data;
+using E_PROJECT_MANAGER.Models;
+
+namespace E_PROJECT_MANAGER.Repository
+{
+    public interface IViTriTuyenDungRepository : IBaseRepository<ViTriTuyenDung>
+    {
+
+    }
+    public class ViTriTuyenDungRepository : BaseRepository<ViTriTuyenDung>, IViTriTuyenDungRepository
+    {
+        public ViTriTuyenDungRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
